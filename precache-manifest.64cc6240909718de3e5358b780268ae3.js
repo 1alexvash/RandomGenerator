a03710b0674ec949833690e20ecafc3ce85405cc
Alexander <1alexvash@gmail.com>
2020-08-17 12:50:10 +0300

    Updates

---
+++ precache-manifest.64cc6240909718de3e5358b780268ae3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7997660eb44fc2d0c0d96dec88f8552",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d2fc866ffdefca80bd3",
+    "url": "/static/css/main.90cca4fe.chunk.css"
+  },
+  {
+    "revision": "da27a5435be51e656ccf",
+    "url": "/static/js/2.b764019c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b764019c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2fc866ffdefca80bd3",
+    "url": "/static/js/main.2ac6d18a.chunk.js"
+  },
+  {
+    "revision": "d41813f4c2c124b09ebd",
+    "url": "/static/js/runtime-main.43f7abea.js"
+  },
+  {
+    "revision": "56418137b848eb989883a7a37df8264b",
+    "url": "/static/media/coinDrop.56418137.mp3"
+  },
+  {
+    "revision": "82fe074e2de1c438d8fdf9afc19c427f",
+    "url": "/static/media/diceRoll.82fe074e.mp3"
+  },
+  {
+    "revision": "bfedd51957c247ca7055ddd42ac4ed8f",
+    "url": "/static/media/head.bfedd519.png"
+  },
+  {
+    "revision": "5d68650ee5ae4440224ab4722d1a0936",
+    "url": "/static/media/menu_coin.5d68650e.png"
+  },
+  {
+    "revision": "7ccb9f14dc03dd3837e1392993c15e2c",
+    "url": "/static/media/menu_dice.7ccb9f14.png"
+  },
+  {
+    "revision": "9bf6c82a5a81ed670004aac529a754a7",
+    "url": "/static/media/menu_numbers.9bf6c82a.png"
+  },
+  {
+    "revision": "2b7b4b6f94a4acd4426cb02b25dcbbc3",
+    "url": "/static/media/tail.2b7b4b6f.png"
+  }
+]);
